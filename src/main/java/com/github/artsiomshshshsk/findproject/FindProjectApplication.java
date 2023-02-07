070dca1106080743a9515fea62aef91301973ebb
artsiomshshshsk <artsiomshablinskiy@gmail.com>
2023-02-07 23:45:27 +0100

    created main domain classes

--- src/main/java/com/github/artsiomshshshsk/findproject/FindProjectApplication.java
+++ src/main/java/com/github/artsiomshshshsk/findproject/FindProjectApplication.java
@@ -2,9 +2,6 @@ package com.github.artsiomshshshsk.findproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class FindProjectApplication {
---
+++ src/main/java/com/github/artsiomshshshsk/findproject/domain/Role.java
@@ -0,0 +1,21 @@
+package com.github.artsiomshshshsk.findproject.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+        @Id
+        private Long id;
+        private String name;
+        @ManyToOne
+        private User assignedUser;
+        private boolean isAvailable;
+}
