603a9a7d111c65f602eeadcd87f08200009521e1
artsiomshshshsk <artsiomshablinskiy@gmail.com>
2023-02-16 16:08:49 +0300

    documentation for auth endpoints

--- src/main/java/com/github/artsiomshshshsk/findproject/config/SpringFoxConfig.java
+++ src/main/java/com/github/artsiomshshshsk/findproject/config/SpringFoxConfig.java
@@ -2,11 +2,10 @@ package com.github.artsiomshshshsk.findproject.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -23,6 +22,21 @@ public class SpringFoxConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+
+    }
+
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Teamder")
+                .description("Api")
+                .contact(new Contact("Artsiom Shablinski",
+                        "https://github.com/artsiomshshshsk",
+                        "artsiomshablinskiy@gmail.com")
+                )
+                .version("1.0")
                 .build();
     }
 
--- src/main/java/com/github/artsiomshshshsk/findproject/security/auth/AuthenticationController.java
+++ src/main/java/com/github/artsiomshshshsk/findproject/security/auth/AuthenticationController.java
@@ -1,18 +1,23 @@
 package com.github.artsiomshshshsk.findproject.security.auth;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@Api(tags = "Authentication")
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
 
   private final AuthenticationService service;
 
+
+  @ApiOperation(value = "Sign-up")
   @PostMapping("/register")
   public ResponseEntity<AuthenticationResponse> register(
           @RequestBody RegisterRequest registerRequest
@@ -20,6 +25,8 @@ public class AuthenticationController {
 
     return ResponseEntity.ok(service.register(registerRequest));
   }
+
+  @ApiOperation(value = "Sign-In")
   @PostMapping("/authenticate")
   public ResponseEntity<AuthenticationResponse> authenticate(
       @RequestBody AuthenticationRequest request ){
