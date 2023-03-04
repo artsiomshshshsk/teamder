be5b540deb48a2d89ddd56c49b1f12af1c58a3a3
artsiomshshshsk <artsiomshablinskiy@gmail.com>
2023-03-04 23:32:06 +0100

    active profiles

--- src/test/java/com/github/artsiomshshshsk/findproject/FindProjectApplicationTests.java
+++ src/test/java/com/github/artsiomshshshsk/findproject/FindProjectApplicationTests.java
@@ -2,7 +2,11 @@ package com.github.artsiomshshshsk.findproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
+
+@ActiveProfiles(profiles = "test")
 @SpringBootTest
 class FindProjectApplicationTests {
 
