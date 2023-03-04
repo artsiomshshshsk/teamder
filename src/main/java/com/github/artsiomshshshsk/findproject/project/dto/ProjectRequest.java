570f64f65b4d4e685fc4cbaf7781d77bbe7b1199
artsiomshshshsk <artsiomshablinskiy@gmail.com>
2023-03-04 13:13:54 +0100

    added constraints on project creation request

--- src/main/java/com/github/artsiomshshshsk/findproject/project/dto/ProjectRequest.java
+++ src/main/java/com/github/artsiomshshshsk/findproject/project/dto/ProjectRequest.java
@@ -5,16 +5,20 @@ import lombok.Builder;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 @Builder
 public record ProjectRequest(
 
         @NotBlank(message = "Project name is mandatory")
+        @Size(max = 15, message = "Project name can be 15 characters max")
         String name,
         @NotBlank(message = "Short description is mandatory")
+        @Size(max = 300, message = "Short description can be 300 characters max")
         String shortDescription,
 
         @NotBlank(message = "Description is mandatory")
+        @Size(max = 1000, message = "Description can be 1000 characters max")
         String description,
 
         @NotNull(message = "Roles are mandatory")
