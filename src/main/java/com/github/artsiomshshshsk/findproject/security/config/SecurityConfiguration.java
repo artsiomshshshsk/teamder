a57d35bc7c151eb639380576e9e4f8a7f394c3a9
artsiomshshshsk <artsiomshablinskiy@gmail.com>
2023-03-14 00:14:52 +0100

    cors

--- src/main/java/com/github/artsiomshshshsk/findproject/security/config/SecurityConfiguration.java
+++ src/main/java/com/github/artsiomshshshsk/findproject/security/config/SecurityConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
@@ -77,6 +78,20 @@ public class SecurityConfiguration {
         .authenticationProvider(authenticationProvider)
         .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
     return http.build();
+  }
+
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Collections.singletonList("*"));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
+    configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+    configuration.setExposedHeaders(Arrays.asList("Content-Type", "Authorization"));
+    configuration.setAllowCredentials(true);
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
 
